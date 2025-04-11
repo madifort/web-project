254583f3879d774032d239a9e3ecbe0812cb8df1
madifort <preshmadu29@gmail.com>
2025-04-11 19:32:37 +0100

    question 1

---
+++ Assignment3/introduction.js
@@ -0,0 +1,8 @@
+// Create variables for personal information
+let name = "Fortune Maduwuike";
+let age = 25;
+let nationality = "Nigerian";
+
+// Combined introduction sentence
+let introduction = `Hello, my name is ${name}, I'm ${age} years old, and I'm ${nationality}.`;
+console.log(introduction);
