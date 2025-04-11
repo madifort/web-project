bf44985be462be86edf46f2e3e4b1d6fcba907ce
madifort <preshmadu29@gmail.com>
2025-04-11 19:35:08 +0100

    question 2

---
+++ Assignment3/transormation.js
@@ -0,0 +1,9 @@
+// Stores favorite quote
+let quote = "The only way to do great work is to love what you do.";
+
+// Converts to uppercase and lowercase
+let uppercaseQuote = quote.toUpperCase();
+let lowercaseQuote = quote.toLowerCase();
+
+console.log("Uppercase:", uppercaseQuote);
+console.log("Lowercase:", lowercaseQuote);
